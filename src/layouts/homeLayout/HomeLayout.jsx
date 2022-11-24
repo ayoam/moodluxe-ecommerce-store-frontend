a9b7c1ef07a7a227ea32f3fab3ea7d6db278bff4
ayoam <you@example.com>
2022-11-24 15:41:44 +0100

    [Window Title]

--- src/layouts/homeLayout/HomeLayout.jsx
+++ src/layouts/homeLayout/HomeLayout.jsx
@@ -28,18 +28,10 @@ const HomeLayout = ({children}) => {
     return (
         <>
             <div className={"flex flex-row-reverse w-full relative"}>
+                <Cart/>
+                <SearchBar/>
                 <div
-                    className={`bg-gray-100 h-screen ${cartIsActive ? "w-full sm:w-[400px]" : "w-0"} transition-all ease-out fixed z-50`}>
-                    <Cart className={`${!cartIsActive && "hidden"}`}/>
-                </div>
-                <div
-                    className={`bg-amber-100 v-screen ${searchIsActive ? "w-full sm:w-full" : "w-0"} transition-all ease-out fixed z-50`}>
-                    <div className={`${!searchIsActive && "hidden"}`}>
-                        <SearchBar/>
-                    </div>
-                </div>
-                <div
-                    className={`w-screen ${cartIsActive && "translate-x-[0px] sm:translate-x-[-400px] transition-transform ease-out"} ${searchIsActive && "translate-x-[0px] translate-y-[400px]"}`}>
+                    className={`w-screen ${cartIsActive && "translate-x-[0px] sm:translate-x-[-400px] transition-transform ease-out"} ${searchIsActive && "translate-x-[0px] translate-y-[550px] duration-700 transition-transform ease-out"}`}>
                     <HomeHeader/>
                     <main>{children}</main>
                     <HomeFooter/>
