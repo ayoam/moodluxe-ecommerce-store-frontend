000d1f17cd805a81591412828f0454377bed0cd9
ayoam <you@example.com>
2022-12-16 11:19:21 +0100

    small fix 5

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,13 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import HomeLayout from "../../layouts/homeLayout/HomeLayout";
 import HeroSection from "../../components/heroSection/HeroSection";
-import CollectionItem from "../../components/collectionItem/CollectionItem";
 import CollectionsSection from "../../components/collectionsSection/CollectionsSection";
 import BestSellerSection from "../../components/bestSellerSection/BestSellerSection";
 import AboutSection from "../../components/aboutSection/AboutSection";
 import NewsLettersSection from "../../components/newslettersSection/NewsLettersSection";
 import ScrollTopButton from "../../components/scrollUpButton/ScrollTopButton";
-import GetProductsByQueryParams from "../../service/productRequests/GetProductsByQueryParams";
 
 const HomePage = ()=>{
 
