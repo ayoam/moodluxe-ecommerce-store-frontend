02fb25872c053e85a15acda614dba1c4fb1ec649
ayoam <you@example.com>
2022-12-28 10:47:17 +0100

    small fix in order-completed page

--- src/pages/orderCompletedPage/OrderCompletedPage.jsx
+++ src/pages/orderCompletedPage/OrderCompletedPage.jsx
@@ -6,12 +6,13 @@ import UnauthorizedPage from "../../components/unauthorizedPage/UnauthorizedPage
 import NotFoundPage from "../404Page/NotFoundPage";
 import HomeLayout from "../../layouts/homeLayout/HomeLayout";
 import {IoMdCheckmarkCircle} from "react-icons/io"
+import {useNavigate} from "react-router-dom";
 
 const OrderCompletedPage = ({order})=>{
     const [cookies, setCookie] = useCookies();
     const user = useRecoilValue(appUserState);
-
-    //if(!cookies["order-number"]) return <NotFoundPage/>
+    const navigate = useNavigate();
+    if(!cookies["order-number"]) return <NotFoundPage/>
 
     return(
         <>
@@ -23,7 +24,7 @@ const OrderCompletedPage = ({order})=>{
                         <p className={"pt-5 font-normal text-2xl"}>Thank you for your purchase ! </p>
                         <p className={"pt-4"}>Your order ID is : {cookies["order-number"]}</p>
                         <p className={"pt-4 text-center sm:px-12 sm:py12"}>You will receive an order confirmation email with your details of your order.</p>
-                        <button className={"mt-16 bg-red-600 py-3 px-6 font-semibold shadow-[1px_1px_20px_3px_rgba(220,38,38,0.6)] hover:bg-transparent hover:border-red-600 border-transparent border-2 hover:backdrop-blur-sm transition-all"}>View orders</button>
+                        <button className={"mt-16 bg-red-600 py-3 px-6 font-semibold shadow-[1px_1px_20px_3px_rgba(220,38,38,0.6)] hover:bg-transparent hover:border-red-600 border-transparent border-2 hover:backdrop-blur-sm transition-all"} onClick={()=>navigate("/myAccount")}>View orders</button>
                     </div>
 
                 </section>
