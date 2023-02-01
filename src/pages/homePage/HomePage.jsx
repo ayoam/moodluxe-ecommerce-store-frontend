39e6a9118de05c55ecd4b98abdf90cec39159dfe
ayoam <you@example.com>
2023-02-01 10:34:56 +0100

    modified home page

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -19,6 +19,7 @@ const HomePage = ()=>{
             <AboutSection/>
             <NewsLettersSection/>
             <ScrollTopButton/>
+            <NewsLetterPopup/>
         </HomeLayout>
     )
 }
